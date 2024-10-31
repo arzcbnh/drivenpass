e1e6e965ddae4d86a58d59858ae8dcc09d1cfc90
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-30 23:39:31 -0300

    Enable error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { json } from "express";
+import { handleError } from "#middleware";
 import * as routes from "#routes";
 
 const app = express();
 app.use(json());
 
 Object.values(routes).forEach(route => app.use(route));
+app.use(handleError);
 
 const port = process.env.PORT ?? 5000;
 app.listen(port, () => console.log("Listening to port " + port));
