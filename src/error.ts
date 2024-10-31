ba92d642f4b0df27d02294d52139770da6fa335c
Matheus Henrique <matthpmoreira@gmail.com>
2024-10-30 23:21:31 -0300

    Create error classes

---
+++ src/error.ts
@@ -0,0 +1,17 @@
+export class ConflictError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export class UserAlreadyExistsError extends ConflictError {
+    constructor(email: string) {
+        super(`Email ${email} already registered.`);
+    }
+}
+
+export class NotFoundError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
